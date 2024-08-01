1b06a2b36309c51ff72f0829b935e5232acbdba5
AmanJoshi4455 <imamanjoshi@gmail.com>
2024-08-01 18:10:03 +0530

    first commit

---
+++ ankit.py
@@ -0,0 +1,73 @@
+# x=int(input("Enter the number: "))
+# r1=x%10
+# x=x//10
+# r2=x%10
+# x=x//10
+# r3=x%10
+# print(r1)
+# print(x)
+# print(r2)
+# print(r3)
+# x=r1*100+r2*10+r3*1
+# print(x)
+# x=int(input("Enter the number: "))
+# r1=x%10
+# x=x//10
+# r2=x%10
+# x=x//10
+# r3=x%10
+# x=x//10
+# print(x)
+# print(r1)
+# print(r2)
+# print(r3)
+# x= r1+r2+r3
+# x=input("Enter the alphabet: ")
+# if(x=="a" or x=="e" or x=="i" or x=="o" or x=="u"):
+#     print("your alphabet is a vovel...")
+# else:
+#     print("your alphabet is a consonant....")
+# x=1
+# while(x<=10):
+#     print(x)
+#     x+=1
+
+# x=int(input("enter the number: "))
+# count=1
+# while(x>=1):
+#     count*=x
+#     x-=1
+# print(count)
+
+# x=int(input("enter the number: "))
+# y=1
+# while(y<=10):
+#     print(x,"*",y,"=",x*y)
+#     y+=1
+# x=1
+# for i in range(1,100):
+#     print(chr(x))
+#     x+=1
+
+# x=int(input("enter the  starting point: "))
+# y=int(input("enter the ending point: "))
+# while(x<=y):
+#     if(x%2==0):
+#         print(x)
+#     x+=1 
+# x=int(input("enter the starting point: "))
+# y=int(input("enter the ending point: ")) 
+# while(x<=y):
+#     if(x%2==0):
+#         print(x)
+#     x+=1
+   
+# x=int(input("enter the starting point: "))
+# y=int(input("enter the ending point: ")) 
+# for i in range(x,y+1):
+#     count=0
+#     for j in range(1,i+1):
+#         if(i%j==0):
+#             count+=1 
+#     if(count==2):
+#         print(i)           
